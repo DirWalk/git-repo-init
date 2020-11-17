c064893e54483e7aa7e5e558284d903e8109e9c7
DirWalk <>
2020-11-17 18:00:50 -0500

    Made functions more modular, fixed Git identity issue, fixed Windows Git-isms

--- git-repo-init.py
+++ git-repo-init.py
@@ -5,10 +5,10 @@ import getpass
 import json
 
 
-def github_repo(arguments, password):
+def github_repo(repo_name, username, password):
     url = 'https://api.github.com/user/repos'
-    data = {"name": arguments.repo_name, "private": False}
-    user = arguments.username
+    data = {"name": repo_name, "private": False}
+    user = username
     ret = requests.post(url, data=json.dumps(data), auth=(user, password))
 
     if ret.status_code != 201:
@@ -16,35 +16,37 @@ def github_repo(arguments, password):
         exit(1)
 
 
-def git_init(arguments, conn, password):
+def git_init(repo_name, username, password, connection_type):
     with open('README.md', 'w') as f:
-        f.write('# {}\n'.format(arguments.repo_name))
+        f.write('# {}\n'.format(repo_name))
     os.system('git init')
     os.system('git add .')
-    os.system('git commit -m "Initial commit"')
+    os.system('git -c user.name="{}" -c user.email="" commit -m "Initial commit"'.format(username))
     os.system('git branch -M main')
 
-    if conn == 'SSH':
-        os.system('git remote add origin git@github.com:{}/{}.git'.format(arguments.username, arguments.repo_name))
-        os.system('git push -u origin master')
-    elif conn == 'HTTPS':
-        os.system('git remote add origin https://github.com/{}/{}.git'.format(arguments.username, arguments.repo_name))
-        os.system('git push https://{}:{}@github.com/{}/{}.git'.format(arguments.username, password, arguments.username,
-                                                                       arguments.repo_name))
+    if connection_type == 'SSH':
+        os.system('git remote add origin git@github.com:{}/{}.git'.format(username, repo_name))
+        os.system('git push -u origin main')
+    elif connection_type == 'HTTPS':
+        os.system('git remote add origin https://github.com/{}/{}.git'.format(username, repo_name))
+        os.system('git push https://{}:{}@github.com/{}/{}'.format(username, password, username, repo_name))
     else:
-        raise ValueError("Unsupported GitHub connection type: {}".format(conn))
+        raise ValueError("Unsupported GitHub connection type: {}".format(connection_type))
 
 
 def run(arguments):
+    repo_name = arguments.repo_name
+    username = arguments.username
     password = getpass.getpass('GitHub Personal Access Token: ')
-    github_repo(arguments, password)
+
+    github_repo(repo_name, username, password)
 
     if args.ssh_mode:
-        conn_mode = 'SSH'
+        connection_type = 'SSH'
     else:
-        conn_mode = 'HTTPS'
+        connection_type = 'HTTPS'
 
-    git_init(arguments, conn_mode, password)
+    git_init(repo_name, username, password, connection_type)
 
 
 if __name__ == '__main__':
